efa381f06c845afdf8c2e088ae6587c9b96ba0c4
devBernardo-Mendes <devbernardomr@gmail.com>
2022-05-04 00:03:00 -0300

    feat: :sparkles: Create app counter for show the use of useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/repositoryList";
 import "./styles/global.scss";
 
 export function App() {
-  return <RepositoryList />;
+  return (
+    <>
+      <RepositoryList />;
+      <Counter />;
+    </>
+  );
 }
